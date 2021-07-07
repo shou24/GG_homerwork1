71902eef674afd148c0d3b8fef2de71ccd69804c
shou24 <85599118+shou24@users.noreply.github.com>
2021-07-07 13:33:13 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("app.js is active");
+
+let btnSubmit = document.getElementById("btn-create");
+
+btnSubmit.addEventListener("click", () => {
+    alert("Playlist sukses dibuat");
+  });
